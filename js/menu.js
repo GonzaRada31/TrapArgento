ec98ee9b3f2e34a9e91d68cea0ff6f19f3c026ca
unknown <cynthiamedina18.08@gmail.com>
2024-05-17 12:36:05 -0300

    responsive pagina multimedia

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-// Obtén los elementos del DOM
-const menuIcon = document.querySelector('.menu-icon');
-const navLinks = document.querySelector('.nav-links');
-
-// Añade un evento de clic al icono del menú
-menuIcon.addEventListener('click', () => {
-    // Alterna la clase 'open' en los enlaces de navegación
-    navLinks.classList.toggle('open');
-});
